77cf6acdec06f0dc04919eb1732b49449f0d00b0
Ben Harris <ben@harrishills.com>
2018-06-11 20:59:02 -0700

    Adding incremental build files

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
 // Safety Goggles ON!
 'use strict';
+
+// Get Key Information
+var initialPage = location;
+var navHome = document.getElementById('home');
+var navSales = document.getElementById('sales'); 
+
+// 1. make my list items -> document.createElement
+var navEl = document.createElement('li');
+
+// 2. use the miles to give the list items some content -> .textContent
+navHome.textContent = 'Home';
+
+// 3. display the list items in the browser -> interact with the DOM -> parentElement.appendChild(childElement)
+ulElement.appendChild(liEl);
