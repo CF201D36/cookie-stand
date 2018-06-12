80411c3f759ac65341e6039e61283ddc36233ba4
Ben Harris <ben@harrishills.com>
2018-06-12 08:30:35 -0700

    Deploying initialbuild code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// ------------------------------------------------------------------------------------------------------------
+// SETUP
+// ------------------------------------------------------------------------------------------------------------
 // Safety Goggles ON!
 'use strict';
 
@@ -12,6 +15,10 @@ var myStore1 = {
   myCustomerMax: 65,
   myAvgSale: 6.3,
   myAvgCustomers: null,
+  myStoreTraffic: function (store, custMin, custMax) {
+    var randNum = Math.random() * (custMax - store.custMin) + custMin;
+    return parseInt(randNum);
+  }
 };
 
 var myStore2 = {
@@ -36,6 +43,7 @@ var myStore4 = {
   myCustomerMax: 38,
   myAvgSale: 2.3,
   myAvgCustomers: null,
+  
 };
 
 var myStore5 = {
@@ -46,26 +54,31 @@ var myStore5 = {
   myAvgCustomers: null,
 };
 
+// Extra Variables
+var myHours = ['6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 pm','1:00 pm','2:00 pm','3:00 pm','4:00 pm','5:00 pm','6:00 pm','7:00 pm','8:00 pm'];
+var myDays  = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
 var myStores = [myStore1, myStore2, myStore3, myStore4, myStore5];
 
-// Calculate and store the AVERAGE number of customers per hour per location
-for (var i = 0; i < myStores.length; i++) {
-  var myArray = myStores[i];
-
-}
-myStore1.myAvgCustomers = parseInt(myStoreTraffic(myStore1.myCustomerMin, myStore1.myCustomerMax));
-myStore2.myAvgCustomers = parseInt(myStoreTraffic(myStore2.myCustomerMin, myStore2.myCustomerMax));
-myStore3.myAvgCustomers = parseInt(myStoreTraffic(myStore3.myCustomerMin, myStore3.myCustomerMax));
-myStore4.myAvgCustomers = parseInt(myStoreTraffic(myStore4.myCustomerMin, myStore4.myCustomerMax));
-myStore5.myAvgCustomers = parseInt(myStoreTraffic(myStore5.myCustomerMin, myStore5.myCustomerMax));
+// ------------------------------------------------------------------------------------------------------------
+// Operations
+// ------------------------------------------------------------------------------------------------------------
+// Estimate the AVERAGE number of CUSTOMERS per HOUR per LOCATION
+myStores.forEach(function(myStore){
+  console.log(myStore);
+  myStore.myAvgCustomers = calcTraffic(myStore.myCustomerMin, myStore.myCustomerMax);
+});
 
 // Calculate and store the simulated amount of cookies purchased each location per hour (myAvgSale * myAvgCustomers)
 
 // Calcluate Average 
 
-// Generate a random number of customers per hour (using RANGE between myCustomerMin and myCustomerMax)
-function myStoreTraffic(store, custMin, custMax) {
-  var randNum = Math.random() * (custMax - store.custMin) + custMin;
+
+// ------------------------------------------------------------------------------------------------------------
+// FUNCTIONS
+// ------------------------------------------------------------------------------------------------------------
+// Generate a random number of customers per hour (uses RANGE between myCustomerMin and myCustomerMax)
+function calcTraffic(custMin, custMax) {
+  var randNum = Math.random() * (custMax - custMin) + custMin;
   return parseInt(randNum);
 }
 
