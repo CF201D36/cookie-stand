c623f0d294ce63a89d979dc128cef176f5bfe0e2
Ben Harris <ben@harrishills.com>
2018-06-15 08:43:39 -0700

    Adding form code, content and styling to current build.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@
 // var myDays  = ['Monday','Tuesday','Wednesday','Thursday','Friday'];
 var myHours  = ['6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 pm','1:00 pm','2:00 pm','3:00 pm','4:00 pm','5:00 pm','6:00 pm','7:00 pm','8:00 pm'];
 var myStores = [];
+var formAddObject = document.getElementById('form-add-object');
 
 // ------------------------------------------------------------------------------------------------------------
 // OBJECTS
@@ -105,6 +106,21 @@ new Store('Alki',           2,  16, 4.6, [], 0); // eslint-disable-line
 
 // Render Table
 Store.buildTable();
+
+// Add the event listener to the form
+Store.Form.addEventListener('submit', Store.addNewStore);
+
+// Setup addNewDog Event Function
+Store.addNewStore = function(event) { // event is a parameter that actually specifies the item being passed -- but it's just a variable.
+  event.preventDefault();
+  var newStoreLocation  = event.target.storeLocation.value;
+  var newCustMin        = event.target.storeCustMin.value;
+  var newCustMax        = event.target.storeCustMax.value;
+  var newAvgSalePerCust = event.target.storeAvgSalePerCust.value;
+
+  new Store(newStoreLocation,newCustMin,newCustMax, newAvgSalePerCust);
+  Store.innerHTML = ' '; // clears the table previously rendered, allowing you to re-render the table in your render function.
+};
 
 // ------------------------------------------------------------------------------------------------------------
 // FUNCTIONS
